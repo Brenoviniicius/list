6d3ccad8d6d80f5c29f50fcc7e4062c52fe9f4e1
duizpisico <brenovmsilva82@gmail.com>
2023-05-14 21:05:58 -0300

    Configuração do CORS

--- src/main/java/com/project/list/projetctions/GameMinProjection.java
+++ src/main/java/com/project/list/projetctions/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.project.list.projetctions;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
